7fa7e508ee8ea4a736a73ea7c69fbbf7398be121
Koposov Denis <kopoden@mail.ru>
2014-11-06 22:47:34 +0300

    The 3rd "release" version of the stack

--- stack_functions.cpp
+++ stack_functions.cpp
@@ -1,24 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 #include "stack_functions.h"
 
 //===========================================================
-STACK_t* stack_ctor ( void ) {
-
-    STACK_t* new_stack = (STACK_t*) calloc (1, sizeof(STACK_t)); // Empty stack
-
-    if (new_stack == 0) { //Not enough memory to allocate
-        free(new_stack->stack_arr);
-        return NULL;
-    }
-	new_stack->stack_arr = (data_t*) calloc (0, sizeof(data_t));
-	
+void stack_ctor (STACK_t* new_stack) {
+    new_stack->stack_arr = NULL;
     new_stack->counter = 0; // Empty stack. Counter points to the zero element
     new_stack->max_size = 0;
 
-    return new_stack; // Stack constructed
+    return; // Stack constructed
 }
 //======================================================
 
@@ -40,29 +28,34 @@ int push (STACK_t* Stack, data_t* Struct) {
 //======================================================
 
 //======================================================
-data_t* pop (STACK_t* Stack) {
+int pop (STACK_t* Stack, data_t* Struct) {
 
     int check = stack_state(Stack);
 
     if (check == EMPTY)
-        return NULL;
+        return EMPTY;
 
     if (check == OUT_OF_STACK)
-       return NULL;
+       return OUT_OF_STACK;
 
     Stack->counter--;
-    return &Stack->stack_arr[Stack->counter];
+    memcpy(Struct, &Stack->stack_arr[Stack->counter], sizeof(data_t));
+    
+    return OK;
 }
 //======================================================
 int stack_resize (STACK_t* Stack, int Size) {
-
-    int check = stack_state(Stack);
-    if (check == OUT_OF_STACK)
+    
+    if (Stack->stack_arr == NULL && (Stack->max_size != 0 || Stack->counter != 0))
         return OUT_OF_STACK;
 
-    Stack->stack_arr = (data_t*) realloc (Stack->stack_arr, sizeof(data_t) * Size);
-    if (Stack->stack_arr == 0 && Size != 0) {
-		free(Stack->stack_arr);
+    if (Stack->stack_arr == NULL)
+        Stack->stack_arr = (data_t*) calloc (Size, sizeof(data_t));
+    else
+        Stack->stack_arr = (data_t*) realloc (Stack->stack_arr, sizeof(data_t) * Size);
+        
+    if (Stack->stack_arr == NULL && Size != 0) {
+        free(Stack->stack_arr);
         return ERR_MEM;
     }
 
@@ -102,15 +95,15 @@ int insrt (STACK_t* Stack, int position, data_t* Struct) {
 int stack_state (STACK_t* Stack) {
 
     if ((Stack->counter < 0) || (Stack->stack_arr == 0) || (Stack->counter > Stack->max_size))
-        return OUT_OF_STACK; //Stack is spoiled (can't push and pop)
+        return OUT_OF_STACK;//Stack is spoiled (can't push and pop)
 
     if (Stack->counter == Stack->max_size)
         return FULL;
 
     if ((Stack->counter == 0) && (Stack->stack_arr > 0))
-        return EMPTY;   //Stack is empty(can't pop, but still can push).
+        return EMPTY;//Stack is empty(can't pop, but still can push).
 
-    return OK;      //Stack is fully workable.
+    return OK;//Stack is fully workable.
 }
 //======================================================
 
@@ -120,10 +113,7 @@ void stack_dtor(STACK_t* Stack) {
 
     free(Stack->stack_arr);
     Stack->stack_arr = NULL;
-    
-    free(Stack);
-	Stack = NULL;
-	
+
     return;
 }
 //=====================================================
--- stack_main.cpp
+++ stack_main.cpp
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "stack_functions.h"
+//#include "stack_functions.h"
 
 int main() {
 
-    STACK_t* strings = stack_ctor();
-    printf("%p\n", strings);
+    STACK_t strings;
+    stack_ctor(&strings);
+    
+    printf("%p\n", strings.stack_arr);
+    printf("%p\n", &strings);
+    
     data_t structure;
-
     structure.last = 4;
     strcpy(structure.String, "Denis");
 
-    if (stack_resize(strings, 1024 * 1024 * 1024 / 104 * 2) == ERR_MEM) {
+    if (stack_resize(&strings, 1024 * 1024 * 1024 / 104 * 2) == ERR_MEM) {
         printf("CAN'T ALLOCATE MEMORY\n");
         return 0;
     }
@@ -21,7 +21,7 @@ int main() {
         printf("ALLOCATION SUCCESSFUL\n");
 
         for (int i = 0; i < 1024 * 1024 * 1024 / 104 * 2 - 1; i++) { // Test 2GB
-            int check = push(strings, &structure);
+            int check = push(&strings, &structure);
 
             if (check == FULL) {
                 printf("STACK IS FULL\n");
@@ -34,16 +34,21 @@ int main() {
         }
     }
     strcpy(structure.String, "Koposov");
-    printf("%d - insert\n", insrt(strings, 0, &structure));//insert test part 1
+    structure.last = 6;
+    printf("%d - insert\n", insrt(&strings, 2, &structure));//insert test part 1
 
-    data_t* answer = pop(strings);
-    printf("%s %d - answer(fixed)\n", answer->String, answer->last); //after POP
+    data_t answer;
+    answer.last = -1;
+    strcpy(answer.String, "empty");
+
+    pop(&strings, &answer);
+    printf("%s %d - answer(fixed)\n", answer.String, answer.last); //after POP
 
     for (int i = 0; i < 10; i++)
-    printf("%s %d\n", strings->stack_arr[i].String, strings->stack_arr[i].last);//insert test part 2
+    printf("%s %d\n", strings.stack_arr[i].String, strings.stack_arr[i].last);//insert test part 2
 
-    stack_dtor(strings);
-    printf("%d\n", sizeof(data_t));// size of 1 structure
+    stack_dtor(&strings);
+    printf("%lu\n", sizeof(data_t));// size of 1 structure
 
     return 0;
 }
